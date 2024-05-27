2b7a8a53953875b8e16a3695be42cc7442d9cd0b
Amine BAAZZOUZ <141878518+AmineBAA@users.noreply.github.com>
2024-05-27 21:26:44 +0100

    Update poc_text.py

--- poc_text.py
+++ poc_text.py
@@ -19,8 +19,27 @@ import pickle
 
 
 # Load your trained model
-classify_claim = pickle.load(open('claims_classifier.pkl', 'rb'))
+model, tokenizer = pickle.load(open('claim_classifier.pkl', 'rb'))
+def classify_claim(claim):
+    encoding = tokenizer.encode_plus(
+        claim,
+        add_special_tokens=True,
+        max_length=max_length,
+        return_token_type_ids=False,
+        padding='max_length',
+        truncation=True,
+        return_attention_mask=True,
+        return_tensors='pt',
+    )
+    input_ids = encoding['input_ids'].to(device)
+    attention_mask = encoding['attention_mask'].to(device)
 
+    with torch.no_grad():
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+        prediction = torch.argmax(logits, dim=-1).item()
+
+    return 'carte' if prediction == 1 else 'autre'
 
 # File uploader
 uploaded_file = st.file_uploader("Choose an Excel file", type=['xlsx'])
